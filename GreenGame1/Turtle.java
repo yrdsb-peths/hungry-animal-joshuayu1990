df52e37a4c461106446b02ea8e315fd8bccfbdb4
joshuayu1990 <101126449+joshuayu1990@users.noreply.github.com>
2022-05-02 22:42:36 -0400

    Green Foot Turtle Move

---
+++ GreenGame1/Turtle.java
@@ -0,0 +1,19 @@
+// WARNING: This file is auto-generated and any changes to it will be overwritten
+import lang.stride.*;
+import java.util.*;
+import greenfoot.*;
+
+/**
+ * 
+ */
+public class Turtle extends Actor
+{
+
+    /**
+     * Act - do whatever the Turtle wants to do. This method is called whenever the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        move(1);
+    }
+}
